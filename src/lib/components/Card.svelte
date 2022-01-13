ffe3c61a92c19f730723cf5f48972db0297d5ee4
Josh Hubbard <jhubbardsf@gmail.com>
2022-01-12 19:06:45 -0500

    Fixing for phones

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -13,10 +13,25 @@
 		border-radius: 10px;
 		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 		transition: 0.3s;
-		width: 40%;
+		width: 80%;
 		padding: 20px;
 		&:hover {
 			box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+		}
+	}
+
+	@media screen and (min-width: 800px) {
+		.card {
+			font-family: 'Roboto';
+			background-color: rgba(255, 255, 255, 1);
+			border-radius: 10px;
+			box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+			transition: 0.3s;
+			width: 40%;
+			padding: 20px;
+			&:hover {
+				box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+			}
 		}
 	}
 </style>
