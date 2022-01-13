f677b5b49d044d2c49ec5de5399b30c6fce0072a
Josh Hubbard <jhubbardsf@gmail.com>
2022-01-13 15:14:41 -0500

    Fixed css in google button

--- src/lib/components/buttons/GoogleLoginButton.svelte
+++ src/lib/components/buttons/GoogleLoginButton.svelte
@@ -3,33 +3,32 @@
 </script>
 
 <div class="google-login" on:click={() => googleSignIn()}>
-	<div class="icon">
-		<img src="google-icon.png" alt="google-icon" />
-	</div>
+	<img src="google-icon.svg" alt="google-icon" />
 	<p>Sign in with Google</p>
 </div>
 
 <style lang="scss">
 	.google-login {
 		cursor: pointer;
-		display: flex;
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
 		margin: 18px auto 0 auto;
 		width: 70%;
 		border: 2px solid black;
 		border-radius: 5px;
 		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-		p {
-			color: #555555;
-			font-weight: bold;
-			margin: auto;
-			padding: 0;
-		}
-		.icon {
-			display: flex;
-			flex-direction: row;
-			padding: 5px;
+		img {
+			// float: left;
+			padding: 7px;
 			border-right: 2px solid black;
-			width: fit-content;
+		}
+		p {
+			grid-column: 2/13;
+			display: flex;
+			align-items: center;
+			margin: 0 auto;
+			font-weight: bold;
+			color: #555555;
 		}
 	}
 </style>
