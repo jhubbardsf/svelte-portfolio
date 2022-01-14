fe9077bf312f498cac8c68262f190653e408e3b2
Josh Hubbard <jhubbardsf@gmail.com>
2022-01-14 17:07:20 -0500

    Fixed hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -8,17 +8,15 @@ const COOKIE_NAME = 'portfolio';
 
 export async function getSession(request: ServerRequest) {
 	console.log("In getSession");
-	console.log("request.locasls", request.locals);
-	const decodedToken: DecodedIdToken | null = request.locals.decodedToken;
+	// const decodedToken: DecodedIdToken | null = request.locals.decodedToken;
+	// console.log({ decodedToken });
+	const cookies = cookie.parse(request.headers.cookie || '');
+	const token = cookies[COOKIE_NAME];
 
-	// const cookies = cookie.parse(request.headers.cookie || '');
-	// const token = cookies[COOKIE_NAME];
-
-	// console.log("session before decodeToken");
-	// const decodedToken = await decodeToken(token);
+	console.log("session before decodeToken");
+	const decodedToken = await decodeToken(token);
 
 	console.log("session after decodeToken");
-	console.log({ decodedToken })
 
 	if (decodedToken) {
 		const { uid, name, email } = decodedToken;
@@ -37,15 +35,15 @@ export const handle: Handle = async ({ request, resolve }) => {
 	request.locals.token = cookies[COOKIE_NAME];
 	console.log("request.locals.token", request.locals.token);
 	// request.locals.decodedToken = await decodeToken(request.locals.toke);
-	const token = request.locals.token;
-	if (token) {
-		request.locals.decodedToken = await decodeToken(token);
-	}
+
 	// endpoint call
 	const response = await resolve(request);
 
 	// after endpoint call
-
+	const token = request.locals.token;
+	if (token) {
+		request.locals.decodedToken = await decodeToken(token);
+	}
 
 	const secure = process.env.NODE_ENV === 'production';
 	const maxAge = 7_200; // (3600 seconds / hour) * 2 hours
