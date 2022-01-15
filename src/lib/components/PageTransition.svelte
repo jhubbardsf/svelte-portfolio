f87e5359d1fc7dea7c37fdee54a76e243f1e316f
Josh Hubbard <jhubbardsf@gmail.com>
2022-01-15 18:35:37 -0500

    Fix for page transition issue

--- src/lib/components/PageTransition.svelte
+++ src/lib/components/PageTransition.svelte
@@ -6,11 +6,23 @@
 	let speed = 300;
 </script>
 
-{#key refresh}
-	<div
-		in:fly={{ x: -width, duration: speed, delay: speed }}
-		out:fly={{ x: width, duration: speed }}
-	>
-		<slot />
-	</div>
-{/key}
+<div class="outer-div">
+	{#key refresh}
+		<div
+			in:fly={{ x: -width, duration: speed, delay: speed }}
+			out:fly={{ x: width, duration: speed }}
+			class="inner-div"
+		>
+			<slot />
+		</div>
+	{/key}
+</div>
+
+<style lang="scss">
+	.outer-div {
+		display: grid;
+		.inner-div {
+			grid-area: 1/1/2/2;
+		}
+	}
+</style>
