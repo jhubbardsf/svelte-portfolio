155e9975fa139a6413302d99dc0dc2cee1351de1
Josh Hubbard <jhubbardsf@gmail.com>
2022-01-15 19:34:33 -0500

    Uploading push notification test

---
+++ src/routes/medications/index.svelte
@@ -0,0 +1,23 @@
+<script>
+	const handleNotification = async () => {
+		let res = await fetch('https://ktzcwkt3bf.execute-api.eu-central-1.amazonaws.com/dev/', {
+			mode: 'no-cors',
+			method: 'POST',
+			headers: {
+				'X-api-key': 'aX8ZqP2g1W5WVQOrAVlro4jmfhZi1BFJ6ws8FHGS',
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			},
+			body: JSON.stringify({
+				title: 'Your awesome site title',
+				body: 'A Must Read New Post',
+				topicId: 'UpAbV3JthuxYALm45UUk',
+				destinationUrl: 'https://example.com/permalink-to-awesome-post'
+			})
+		});
+	};
+</script>
+
+<h1>Testing Medications Page</h1>
+
+<button on:click={handleNotification}>Send notification</button>
