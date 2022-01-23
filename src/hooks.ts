bc0921476a766cf2712de59ca568c428238d4ceb
Josh Hubbard <jhubbardsf@gmail.com>
2022-01-23 17:17:07 -0500

    Removing event checkers that break netlify

--- src/hooks.ts
+++ src/hooks.ts
@@ -11,13 +11,12 @@ export async function getSession(event: RequestEvent) {
 	// Test vercel deploy
 	let decodedToken;
 
-	if (event) {
-		const cookies = cookie.parse(event.request.headers.get('cookie') || '');
+	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
 
-		const token = cookies[COOKIE_NAME];
+	const token = cookies[COOKIE_NAME];
 
-		decodedToken = await decodeToken(token);
-	}
+	decodedToken = await decodeToken(token);
+
 
 
 	if (decodedToken) {
@@ -32,27 +31,25 @@ export async function getSession(event: RequestEvent) {
 export async function handle({ event, resolve }) {
 	console.log("In handle");
 
-	if (event) {
-		const cookies = cookie.parse(event?.request.headers.get('cookie') || '');
+	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
 
-		// before endpoint call
-		event.locals.token = cookies[COOKIE_NAME];
-	}
+	// before endpoint call
+	event.locals.token = cookies[COOKIE_NAME];
+
 
 	// endpoint call
 	const response: Response = await resolve(event);
 
-	if (event) {
-		// after endpoint call
-		const token = event?.locals.token;
+	// after endpoint call
+	const token = event.locals.token;
 
-		const secure = process.env.NODE_ENV === 'production';
-		const maxAge = 7_200; // (3600 seconds / hour) * 2 hours
-		const sameSite = 'Strict';
-		const setCookieValue = `${COOKIE_NAME}=${token || ''}; Max-Age=${maxAge}; Path=/; ${secure ? 'Secure;' : ''
-			} HttpOnly; SameSite=${sameSite}`;
+	const secure = process.env.NODE_ENV === 'production';
+	const maxAge = 7_200; // (3600 seconds / hour) * 2 hours
+	const sameSite = 'Strict';
+	const setCookieValue = `${COOKIE_NAME}=${token || ''}; Max-Age=${maxAge}; Path=/; ${secure ? 'Secure;' : ''
+		} HttpOnly; SameSite=${sameSite}`;
 
-		response.headers.set('Set-Cookie', setCookieValue);
-	}
+	response.headers.set('Set-Cookie', setCookieValue);
+
 	return response;
 };
