deb011fede4ec43cc96419a40e19b6d37f1f4bf2
Josh Hubbard <jhubbardsf@gmail.com>
2022-01-23 16:16:43 -0500

    Fixed hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,11 +1,11 @@
 import type { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
 import type { RequestEvent } from '@sveltejs/kit/types/hooks';
-import type { Handle, GetSession } from '@sveltejs/kit';
 import { decodeToken } from '$lib/server/firebase';
 import cookie from 'cookie';
 
 const COOKIE_NAME = 'portfolio';
 
+/** @type {import('@sveltejs/kit').GetSession} */
 export async function getSession(event: RequestEvent) {
 	console.log("In getSession");
 	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
@@ -21,18 +21,19 @@ export async function getSession(event: RequestEvent) {
 	}
 }
 
-export const handle: Handle = async ({ request, resolve }) => {
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
 	console.log("In handle");
-	const cookies = cookie.parse(request.headers.cookie || '');
+	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
 
 	// before endpoint call
-	request.locals.token = cookies[COOKIE_NAME];
+	event.locals.token = cookies[COOKIE_NAME];
 
 	// endpoint call
-	const response = await resolve(request);
+	const response: Response = await resolve(event);
 
 	// after endpoint call
-	const token = request.locals.token;
+	const token = event.locals.token;
 
 	const secure = process.env.NODE_ENV === 'production';
 	const maxAge = 7_200; // (3600 seconds / hour) * 2 hours
@@ -40,11 +41,7 @@ export const handle: Handle = async ({ request, resolve }) => {
 	const setCookieValue = `${COOKIE_NAME}=${token || ''}; Max-Age=${maxAge}; Path=/; ${secure ? 'Secure;' : ''
 		} HttpOnly; SameSite=${sameSite}`;
 
-	return {
-		...response,
-		headers: {
-			...response.headers,
-			'Set-Cookie': setCookieValue
-		}
-	}
+	response.headers.set('Set-Cookie', setCookieValue);
+
+	return response;
 };
